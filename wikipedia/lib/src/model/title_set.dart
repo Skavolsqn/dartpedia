6a9126502e10afd8d431dbaaaf4bd9ca6fff9bd9
EoPsyco22 <rafaels.pinto05@gmail.com>
2026-07-28 18:07:32 +0000

    Lição 9 Trabalhar com dados JSON, Tarefa 1: Criar o pacote da Wikipédia, Tarefa 2: Configurar um espaço de trabalho Dart, Tarefa 3: Criar a classe Summary, Tarefa 4: Criar a classe TitleSet, Tarefa 5: Criar a classe Artigo, Tarefa 6: Crie a classe SearchResults

---
+++ wikipedia/lib/src/model/title_set.dart
@@ -0,0 +1,42 @@
+class TitlesSet {
+  /// Returns a new [TitlesSet] instance.
+  TitlesSet({
+    required this.canonical,
+    required this.normalized,
+    required this.display,
+  });
+
+  /// the DB key (non-prefixed), e.g. may have _ instead of spaces,
+  /// best for making request URIs, still requires Percent-encoding
+  String canonical;
+
+  /// the normalized title
+  String normalized;
+
+  /// the title as it should be displayed to the user
+  String display;
+
+  /// Returns a new [TitlesSet] instance and imports its values from a JSON map
+  static TitlesSet fromJson(Map<String, Object?> json) {
+    if (json case {
+      'canonical': final String canonical,
+      'normalized': final String normalized,
+      'display': final String display,
+    }) {
+      return TitlesSet(
+        canonical: canonical,
+        normalized: normalized,
+        display: display,
+      );
+    }
+    throw FormatException('Could not deserialize TitleSet, json=$json');
+  }
+
+  @override
+  String toString() =>
+      'TitlesSet['
+      'canonical=$canonical, '
+      'normalized=$normalized, '
+      'display=$display'
+      ']';
+}
